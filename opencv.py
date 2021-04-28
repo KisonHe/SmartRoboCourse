4521668c942e61f3559a28153f8e9a4481193681
KisonHe <kisonmaxine@gmail.com>
2021-04-28 18:40:42 +0800

    V0.1

--- opencv.py
+++ opencv.py
@@ -3,42 +3,105 @@
 # Works with OpenCV4
 # Does NOT work with OpenCV3
 
+# Basic idea
+# We crop the image first.
+# Crop image into upper 3 rings, and lower 3 objects.
+#   PS: Do the deep copy `crop_img = img[y:y+h, x:x+w].copy()`
+#       Not the `crop_img = img[y:y+h, x:x+w]`
+# Then, the upper layer use `new_r = ((r - b) + (r - g))/2`
+# to get rid of white, extract the color we want.
+# The 4 yellow tapes at the corner is annoying, make sure they are cropped out.
+
+# As for the lower part, background isn't white. try it at the field
 
 # import packages
-import imutils
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
 from enum import Enum
 
 
+class Crop:
+    def __init__(self, y1, y2, x1, x2):
+        self.y = y1
+        self.y_h = y2
+        self.x = x1
+        self.x_w = x2
+
+    y = 0
+    y_h = 0
+    x = 0
+    x_w = 0
+
+
+CycCrop = Crop(220, 350, 400, 920)
+RecCrop = Crop(471, 522, 774, 1040)
+
+
+class CGs:
+    RedX = 0
+    GreenX = 0
+    BlueX = 0
+
+
+CycCGs = CGs()
+RecCGs = CGs()
+
+
 class MissionType_t(Enum):
     Unknown = 0
     BRG = 1
     GBR = 2
+    RGB = 3
 
 
 # All status of the status machine
 class Status_t(Enum):
     start = 0
+    Cropping = 3
     findQRCode = 1
-    findCGofRedCyc = 2
-    findCGofBlueCyc = 3
-    findCGofGreenCyc = 4
-    findCGofRedRec = 5
-    findCGofBlueRec = 6
-    findCGofGreenRec = 7
+    findCGofCyc = 2
+    findCGofRec = 5
     TCP2DownMachine = 8
     Finished = 9
 
 
 Status = Status_t.start
+# Status = Status_t.findCGofCyc
+# Status = Status_t.findCGofRec
+# Status = Status_t.Cropping
 MissionType = MissionType_t.Unknown
 
 cap = cv2.VideoCapture("/dev/video2")
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
+
+def getOrder(vR,vG,vB):
+    ret = ''
+    if vR == min(vR, vB, vG):
+        ret += 'R'
+    elif vG == min(vR, vB, vG):
+        ret += 'G'
+    elif vB == min(vR, vB, vG):
+        ret += 'B'
+
+    if min(vR, vB, vG) < vR < max(vR, vB, vG):
+        ret += 'R'
+    elif min(vR, vB, vG) < vG < max(vR, vB, vG):
+        ret += 'G'
+    elif min(vR, vB, vG) < vB < max(vR, vB, vG):
+        ret += 'B'
+
+    if vR == max(vR, vB, vG):
+        ret += 'R'
+    elif vG == max(vR, vB, vG):
+        ret += 'G'
+    elif vB == max(vR, vB, vG):
+        ret += 'B'
+    return ret
+    
+    
 
 def extractColor(var_r, var_g, var_b):
     var_b = var_b.astype(np.int16)
@@ -71,120 +134,172 @@ def extractColor(var_r, var_g, var_b):
 
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture("/dev/video2")
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-
     while Status != Status_t.Finished:
         if Status == Status_t.start:
             # FIXME Init all stuff
             # Drop first 10 frames to avoid rubbish
-            for i in range(0, 10):
+            ret, img = cap.read()
+            while img is None:
+                print("Rubbish")
                 ret, img = cap.read()
             Status = Status_t.findQRCode
             pass
+        elif Status == Status_t.Cropping:
+            img = cv2.imread("a.jpg")
+            # ret, img = cap.read()
+            # img = img[CycCrop.y:CycCrop.y_h, CycCrop.x:CycCrop.x_w]
+            cv2.imshow("image", img)
+            key = cv2.waitKey(1)
+            if key == ord("q"):
+                break
         elif Status == Status_t.findQRCode:
             qrCodeDetector = cv2.QRCodeDetector()
-            cnt = 0
             ret, img = cap.read()
+            cv2.imshow("original", img)
+            cv2.waitKey(1)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             for barcode in decode(gray):
-                print(cnt)
-                cnt += 1
                 print(barcode.data.decode('utf-8'))
                 if barcode.data.decode('utf-8') == "蓝-红-绿":
                     MissionType = MissionType_t.BRG
-                    Status = Status_t.findCGofRedCyc
+                    Status = Status_t.findCGofCyc
                 elif barcode.data.decode('utf-8') == "绿-蓝-红":
                     MissionType = MissionType_t.GBR
-                    Status = Status_t.findCGofRedCyc
-                elif barcode.data.decode('utf-8') == "FIXME":
-                    # FIXME
-                    Status = Status_t.findCGofRedCyc
+                    Status = Status_t.findCGofCyc
+                elif barcode.data.decode('utf-8') == "红-绿-蓝":
+                    MissionType = MissionType_t.GBR
+                    Status = Status_t.findCGofCyc
                     pass
                 else:
                     # FIXME What if there were another QR code on the Filed?
                     raise ValueError('Check your Mission Type')
-            key = cv2.waitKey(1)
-            if key == ord("q"):
-                break
-
-            cap.release()
-            cv2.destroyAllWindows()
-
-# Basic idea
-# We crop the image first.
-# Crop image into upper 3 rings, and lower 3 objects.
-#   PS: Do the deep copy `crop_img = img[y:y+h, x:x+w].copy().copy()`
-#       Not the `crop_img = img[y:y+h, x:x+w]`
-# Then, the upper layer use `new_r = ((r - b) + (r - g))/2`
-# to get rid of white, extract the color we want.
-# The 4 yellow tapes at the corner is annoying, make sure they are cropped out.
-
-# As for the lower part, background isn't white. try it at the field
-
-while 1:
-    # ret, image = cap.read()
-    image = cv2.imread("a.jpg")
-
-    # r, g, b = image[:, :, 0], image[:, :, 1], image[:, :, 2]
-    crop = image[220:350, 400:920].copy()
-    b, g, r = crop[:, :, 0], crop[:, :, 1], crop[:, :, 2]
-    new_r, new_g, new_b = extractColor(r, g, b)
-
-    thresh = cv2.threshold(new_r, 40, 255, cv2.THRESH_BINARY)[1]
-    thresh = cv2.medianBlur(thresh, 5)
-    cv2.imshow("crop", crop)
-    cv2.imshow("new_r", new_r)
-    cv2.imshow("original", image)
-    cv2.imshow("thresh", thresh)
-    cv2.waitKey(1000)
-
-    # find contours in the thresholded image and initialize the
-    # shape detector
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    if contours is not None:
-        # find the biggest countour (c) by the area
-        c = max(contours, key=cv2.contourArea)
-        # print(cv2.contourArea(c))
-        if (cv2.contourArea(c) < 50):
-            # FIXME If the area of c too small, we are not detecting the right thing. Drop this frame.
+                pass
             pass
-        # How to calculate CG of contours
-        # https://docs.opencv.org/master/dd/d49/tutorial_py_contour_features.html
-        M = cv2.moments(c)
-        cx = int(M['m10'] / M['m00'])
-        cy = int(M['m01'] / M['m00'])
-        print(cx)
-        print(cy)
-    # cnts = imutils.grab_contours(cnts)
-    # sd = ShapeDetector()
+        elif Status == Status_t.findCGofCyc:
+            # ret, image = cap.read()
+            image = cv2.imread("a.jpg")
+            crop = image[CycCrop.y:CycCrop.y_h, CycCrop.x:CycCrop.x_w].copy()
+            b, g, r = crop[:, :, 0], crop[:, :, 1], crop[:, :, 2]
+            new_r, new_g, new_b = extractColor(r, g, b)
 
-    # # loop over the contours
-    # if cnts is not None:
-    #     for c in cnts:
-    #         # compute the center of the contour, then detect the name of the
-    #         # shape using only the contour
-    #         cX = 0
-    #         cY = 0
-    #         M = cv2.moments(c)
-    #         if M["m00"] != 0:
-    #             cX = int((M["m10"] / M["m00"]) * ratio)
-    #             cY = int((M["m01"] / M["m00"]) * ratio)
-    #         shape = sd.detect(c)
-    #
-    #         # multiply the contour (x, y)-coordinates by the resize ratio,
-    #         # then draw the contours and the name of the shape on the image
-    #         c = c.astype("float")
-    #         c *= ratio
-    #         c = c.astype("int")
-    #         cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-    #         cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-    #                     0.5, (255, 255, 255), 2)
-    #
-    #         # show the output image
-    #         cv2.imshow("Image", image)
-    #         cv2.waitKey(1)
+            if CycCGs.RedX == 0:
+                # thresh = cv2.adaptiveThreshold(new_r, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 2)
+                retVal, thresh = cv2.threshold(new_r, 40, 255, cv2.THRESH_BINARY)
+                thresh = cv2.medianBlur(thresh, 5)
+            elif CycCGs.GreenX == 0:
+                # thresh = cv2.adaptiveThreshold(new_r, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 2)
+                retVal, thresh = cv2.threshold(new_g, 20, 255, cv2.THRESH_BINARY)
+                # thresh = cv2.medianBlur(thresh, 5)
+            elif CycCGs.BlueX == 0:
+                # thresh = cv2.adaptiveThreshold(new_r, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 2)
+                retVal, thresh = cv2.threshold(new_b, 20, 255, cv2.THRESH_BINARY)
+                # thresh = cv2.medianBlur(thresh, 5)
+            else:
+                print("CycRed:{}".format(CycCGs.RedX))
+                print("CycGreen:{}".format(CycCGs.GreenX))
+                print("CycBlue:{}".format(CycCGs.BlueX))
+                Status = Status_t.findCGofRec
+                continue
+
+            contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+            if contours:
+                # find the biggest countour (c) by the area
+                c = max(contours, key=cv2.contourArea)
+                # print(cv2.contourArea(c))
+                if cv2.contourArea(c) > 50:
+                    M = cv2.moments(c)
+                    cx = int(M['m10'] / M['m00'])
+                    if CycCGs.RedX == 0:
+                        CycCGs.RedX = cx
+                    elif CycCGs.GreenX == 0:
+                        CycCGs.GreenX = cx
+                    elif CycCGs.BlueX == 0:
+                        CycCGs.BlueX = cx
+                    pass
+                else:
+                    # FIXME If the area of c too small, we are not detecting the right thing. Drop this frame.
+                    pass
+                # How to calculate CG of contours
+                # https://docs.opencv.org/master/dd/d49/tutorial_py_contour_features.html
+
+            # cv2.imshow("crop", crop)
+            # cv2.imshow("new_b", new_b)
+            # cv2.imshow("new_r", new_r)
+            # cv2.imshow("new_g", new_g)
+            # cv2.imshow("original", image)
+            # cv2.imshow("thresh", thresh)
+            # cv2.waitKey(1)
+
+
+            # cap.release()
+            # cv2.destroyAllWindows()
+            pass
+        elif Status == Status_t.findCGofRec:
+            # ret, image = cap.read()
+            image = cv2.imread("a.jpg")
+            crop = image[RecCrop.y:RecCrop.y_h, RecCrop.x:RecCrop.x_w].copy()
+            b, g, r = crop[:, :, 0], crop[:, :, 1], crop[:, :, 2]
+            new_r, new_g, new_b = extractColor(r, g, b)
+
+            if RecCGs.RedX == 0:
+                # thresh = cv2.adaptiveThreshold(new_r, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 2)
+                retVal, thresh = cv2.threshold(new_r, 40, 255, cv2.THRESH_BINARY)
+                thresh = cv2.medianBlur(thresh, 5)
+            elif RecCGs.GreenX == 0:
+                # thresh = cv2.adaptiveThreshold(new_r, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 2)
+                retVal, thresh = cv2.threshold(new_g, 20, 255, cv2.THRESH_BINARY)
+                # thresh = cv2.medianBlur(thresh, 5)
+            elif RecCGs.BlueX == 0:
+                # thresh = cv2.adaptiveThreshold(new_r, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 2)
+                retVal, thresh = cv2.threshold(new_b, 20, 255, cv2.THRESH_BINARY)
+                # thresh = cv2.medianBlur(thresh, 5)
+            else:
+                print("RecRed:{}".format(RecCGs.RedX))
+                print("RecGreen:{}".format(RecCGs.GreenX))
+                print("RecBlue:{}".format(RecCGs.BlueX))
+                Status = Status_t.TCP2DownMachine
+                continue
+
+            contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+            if contours:
+                # find the biggest countour (c) by the area
+                c = max(contours, key=cv2.contourArea)
+                # print(cv2.contourArea(c))
+                if cv2.contourArea(c) > 50:
+                    M = cv2.moments(c)
+                    cx = int(M['m10'] / M['m00'])
+                    if RecCGs.RedX == 0:
+                        RecCGs.RedX = cx
+                    elif RecCGs.GreenX == 0:
+                        RecCGs.GreenX = cx
+                    elif RecCGs.BlueX == 0:
+                        RecCGs.BlueX = cx
+                    pass
+                else:
+                    # FIXME If the area of c too small, we are not detecting the right thing. Drop this frame.
+                    pass
+                # How to calculate CG of contours
+                # https://docs.opencv.org/master/dd/d49/tutorial_py_contour_features.html
+
+            cv2.imshow("crop", crop)
+            # cv2.imshow("new_b", new_b)
+            # cv2.imshow("new_r", new_r)
+            # cv2.imshow("new_g", new_g)
+            cv2.imshow("original", image)
+            # cv2.imshow("thresh", thresh)
+            cv2.waitKey(1)
+
+            pass
+        elif Status == Status_t.TCP2DownMachine:
+            print("圆环左到右顺序为{}".format(getOrder(CycCGs.RedX, CycCGs.GreenX, CycCGs.BlueX)))
+            print("物品左到右顺序为{}".format(getOrder(RecCGs.RedX, RecCGs.GreenX, RecCGs.BlueX)))
+            Status = Status_t.Finished
+
+
+
+
 
 # 霍夫变换
 # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
