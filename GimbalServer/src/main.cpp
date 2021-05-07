52fbcad3402c67b9847da6437c364b0b8bf7a29d
KisonHe <kisonmaxine@gmail.com>
2021-05-07 16:34:36 +0800

    Fix bugs

--- GimbalServer/src/main.cpp
+++ GimbalServer/src/main.cpp
@@ -59,6 +59,7 @@ void setup()
 {
     Serial.begin(115200);
     memset(Positions,0,sizeof(Positions));
+    servo_init();
     esp_err_t err = nvs_flash_init();
     if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         // NVS partition was truncated and needs to be erased
--- GimbalServer/src/servo_warpper.h
+++ GimbalServer/src/servo_warpper.h
@@ -1,10 +1,10 @@
 #ifndef SERVO_WARPPER_H
 #define SERVO_WARPPER_H
 #include <ESP32Servo.h>
-const int servoXPin = 34;
-const int servoYPin = 35;
+const int servoXPin = 26;
+const int servoYPin = 25;
 
 extern Servo Servo_X;
 extern Servo Servo_Y;
-
+void servo_init();
 #endif
